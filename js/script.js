62fdc34c75f63cc68cfbe7c539fa7ef44a352223
rlindentsieni <rilindentsieni@gmail.com>
2024-09-14 22:53:01 +0200

    File deleted and moved to assests folder

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  Particles.init({
+    selector: ".particles",
+    color: ['#b1dcf5','#f5b1c2'],
+    connectParticles: true,
+    maxParticles: 50,
+    sizeVariations: 30,
+    speed: 0.7,
+  });
+};
